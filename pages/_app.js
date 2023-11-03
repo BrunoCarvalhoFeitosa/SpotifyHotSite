8846e1f071ec5bd92f1603b3bf4739d2a596ee67
Bruno Carvalho Feitosa <brunocarvalhofeitosa@outlook.com>
2023-11-02 23:51:55 -0300

    fix: change fav

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import Head from "next/head"
-import favicon from "../public/favicon.ico"
+import favicon from "../public/fav.png"
 import "../styles/Globals/globals.css"
 
 const MyApp = ({ Component, pageProps }) => {
@@ -11,9 +11,6 @@ const MyApp = ({ Component, pageProps }) => {
         <meta name="author" content="Bruno Carvalho Feitosa" />
         <meta name="description" content="Spotify, música com qualidade para todos." />
         <meta name="theme-color" content="#43CD80" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href={favicon.src}/>
         <title>Spotify - Música para todos</title>
       </Head> 
